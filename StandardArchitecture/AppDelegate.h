7976c8c49c1df2b10d50453e6273a14caadc2b78
daniulaolu <287929070@qq.com>
2017-07-11 15:17:05 +0800

    Initial Commit

---
+++ StandardArchitecture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StandardArchitecture
+//
+//  Created by lujh on 2017/7/11.
+//  Copyright © 2017年 lujh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StandardArchitecture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StandardArchitecture
+//
+//  Created by lujh on 2017/7/11.
+//  Copyright © 2017年 lujh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
